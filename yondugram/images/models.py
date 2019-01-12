2454caa088f2028d97f89464f92b72d762c070f7
yongseondu <weed0124@naver.com>
2019-01-12 17:32:32 +0900

    2019-01-12 Models Create

--- yondugram/images/models.py
+++ yondugram/images/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from yondugram.users import models as user_models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField(null=True)
+    location = models.CharField(max_length=140, null=True)
+    caption = models.TextField(null=True)
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+class Like(TimeStampedModel):
+
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
