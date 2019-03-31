03033deda60fde29e8ab10eb3a0405510311a0dd
yongseondu <weed0124@naver.com>
2019-03-31 16:35:01 +0900

    React Context and Translating the Footer. State Component

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,28 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
-const Footer = (props, context) => (
-    <footer className={styles.footer}>
-        <div className={styles.column}>
-            <nav className={styles.nav}>
-                <ul className={styles.list}>
-                    <li className={styles.listItem}>About Us</li>
-                    <li className={styles.listItem}>Support</li>
-                    <li className={styles.listItem}>Blog</li>
-                    <li className={styles.listItem}>Press</li>
-                    <li className={styles.listItem}>API</li>
-                    <li className={styles.listItem}>Jobs</li>
-                    <li className={styles.listItem}>Privacy</li>
-                    <li className={styles.listItem}>Terms</li>
-                    <li className={styles.listItem}>Directory</li>
-                    <li className={styles.listItem}>Language</li>
-                </ul>
-            </nav>
-        </div>
-        <div className={styles.column}>
-            <span className={styles.copyright}>© 2019 yondugram</span>
-        </div>
-    </footer>
-)
+class Footer extends React.Component {
+    static contextTypes = {
+        t: PropTypes.func.isRequired
+    }
+    render() {
+        return (
+            <footer className={styles.footer}>
+                <div className={styles.column}>
+                    <nav className={styles.nav}>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}>{this.context.t("About Us")}</li>
+                            <li className={styles.listItem}>{this.context.t("Support")}</li>
+                            <li className={styles.listItem}>{this.context.t("Blog")}</li>
+                            <li className={styles.listItem}>{this.context.t("Press")}</li>
+                            <li className={styles.listItem}>{this.context.t("API")}</li>
+                            <li className={styles.listItem}>{this.context.t("Jobs")}</li>
+                            <li className={styles.listItem}>{this.context.t("Privacy")}</li>
+                            <li className={styles.listItem}>{this.context.t("Terms")}</li>
+                            <li className={styles.listItem}>{this.context.t("Directory")}</li>
+                            <li className={styles.listItem}>{this.context.t("Language")}</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={styles.column}>
+                    <span className={styles.copyright}>© 2019 yondugram</span>
+                </div>
+            </footer>
+        )
+    }
+}
+    
+
 
 export default Footer;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,14 @@
 export const translations = {
-    en: {
-        "Login in":"Logged In"
+    kr: {
+        "Login in":"로그인",
+        "About Us":"Yondugram 정보",
+        "Support":"지원",
+        "Blog":"홍보 센터",
+        "Press":"API",
+        "Jobs":"채용 정보",
+        "Privacy":"개인정보처리방침",
+        "Terms":"약관",
+        "Directory":"디렉터리",
+        "Language":"언어"
     }
 }
