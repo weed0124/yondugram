3975c8d23b90b8c2bc08ef68f45ab718fd0909e6
yongseondu <weed0124@naver.com>
2019-03-31 16:46:17 +0900

    Stateless Components and Context

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,37 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./styles.scss";
 
-class Footer extends React.Component {
-    static contextTypes = {
-        t: PropTypes.func.isRequired
-    }
-    render() {
-        return (
-            <footer className={styles.footer}>
-                <div className={styles.column}>
-                    <nav className={styles.nav}>
-                        <ul className={styles.list}>
-                            <li className={styles.listItem}>{this.context.t("About Us")}</li>
-                            <li className={styles.listItem}>{this.context.t("Support")}</li>
-                            <li className={styles.listItem}>{this.context.t("Blog")}</li>
-                            <li className={styles.listItem}>{this.context.t("Press")}</li>
-                            <li className={styles.listItem}>{this.context.t("API")}</li>
-                            <li className={styles.listItem}>{this.context.t("Jobs")}</li>
-                            <li className={styles.listItem}>{this.context.t("Privacy")}</li>
-                            <li className={styles.listItem}>{this.context.t("Terms")}</li>
-                            <li className={styles.listItem}>{this.context.t("Directory")}</li>
-                            <li className={styles.listItem}>{this.context.t("Language")}</li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className={styles.column}>
-                    <span className={styles.copyright}>© 2019 yondugram</span>
-                </div>
-            </footer>
-        )
-    }
-}
-    
+const Footer = (props, context) => (
+    <footer className={styles.footer}>
+        <div className={styles.column}>
+            <nav className={styles.nav}>
+                <ul className={styles.list}>
+                    <li className={styles.listItem}>{context.t("About Us")}</li>
+                    <li className={styles.listItem}>{context.t("Support")}</li>
+                    <li className={styles.listItem}>{context.t("Blog")}</li>
+                    <li className={styles.listItem}>{context.t("Press")}</li>
+                    <li className={styles.listItem}>{context.t("API")}</li>
+                    <li className={styles.listItem}>{context.t("Jobs")}</li>
+                    <li className={styles.listItem}>{context.t("Privacy")}</li>
+                    <li className={styles.listItem}>{context.t("Terms")}</li>
+                    <li className={styles.listItem}>{context.t("Directory")}</li>
+                    <li className={styles.listItem}>{context.t("Language")}</li>
+                </ul>
+            </nav>
+        </div>
+        <div className={styles.column}>
+            <span className={styles.copyright}>© 2019 yondugram</span>
+        </div>
+    </footer>
+)
 
+Footer.contextTypes = {
+    t: PropTypes.func.isRequired
+}
 
 export default Footer;
